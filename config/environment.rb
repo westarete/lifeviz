0c44b3f4d344f4d0f07dd8b92c2724922717ed0a
Clinton Judy <clinton@westarete.com>
2009-12-08 09:58:52 -0500

    Added awesome_nested_set to gems.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -13,6 +13,8 @@ Rails::Initializer.run do |config|
   config.gem 'webrat'
   config.gem 'cucumber'
   config.gem 'haml'
+  
+  config.gem 'awesome_nested_set', :version => '1.4.3'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
