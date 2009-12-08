3a45e23eb6e3487c0a22ede8349433335adc2b49
Clinton Judy <clinton@westarete.com>
2009-12-08 16:49:36 -0500

    Added methods to load seed data. Currently importing taxonomy.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+require 'progressbar'
+require 'db/seed_methods'
+include SeedMethods
+UBIOTA = "db/data/ubiota_taxonomy.psv"
+ANAGE = "db/data/anage.xml"
+
+
+# Count the number of rows in a file.
+def num_lines(filename)
+  num_lines = 0
+  File.open(filename).each do |line|
+    id, term, rank, hierarchy, parent_id, num_children, hierarchy_ids = line.split("|")
+    next if rank == "rank"
+    break if rank == "6"
+    num_lines += 1
+  end
+  num_lines
+end
+
+
+# DESTROY ALL TAXONOMY! BUAH HUA HA HA HAHAHAHAHA
+print "Destroying all taxonomy..."
+Taxon.destroy_all
+puts "done."
+
+# Load new taxonomy information from UBioTa.
+progress "Loading seed data...", num_lines(UBIOTA) do |progress_bar|
+  File.open(UBIOTA).each do |line|
+    id, term, rank, hierarchy, parent_id, num_children, hierarchy_ids = line.split("|")
+    next if rank == "rank"
+    break if rank == "6"
+    Taxon.create!(:id => id, :name => term, :rank => rank, :parent_id => parent_id)
+    progress_bar.inc
+  end
+end
+
+
+
