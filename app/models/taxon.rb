09cde8bdc2554d22d671a583897a35dd81394b53
Clinton Judy <clinton@westarete.com>
2009-12-09 12:49:41 -0500

    Got seeds.rb to import taxonomy correctly. Hooray.

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -12,4 +12,5 @@
 #
 
 class Taxon < ActiveRecord::Base
+  acts_as_nested_set
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,22 +17,23 @@ def num_lines(filename)
   num_lines
 end
 
-
-# DESTROY ALL TAXONOMY! BUAH HUA HA HA HAHAHAHAHA
-print "Destroying all taxonomy..."
-Taxon.destroy_all
-puts "done."
-
 # Load new taxonomy information from UBioTa.
 progress "Loading seed data...", num_lines(UBIOTA) do |progress_bar|
   File.open(UBIOTA).each do |line|
     id, term, rank, hierarchy, parent_id, num_children, hierarchy_ids = line.split("|")
     next if rank == "rank"
     break if rank == "6"
-    Taxon.create!(:id => id, :name => term, :rank => rank, :parent_id => parent_id)
+    taxon = Taxon.new
+    taxon.id = id.to_i
+    taxon.name = term
+    taxon.rank = rank.to_i
+    if parent_id == "-1"
+      taxon.parent_id = nil
+    else
+      taxon.parent_id = parent_id.to_i
+    end
+    taxon.send(:create_without_callbacks)
     progress_bar.inc
   end
 end
-
-
 
