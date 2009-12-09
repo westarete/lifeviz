4711681fbc46bc94037cbd1844e3cdc089cc5c62
Clinton Judy <clinton@westarete.com>
2009-12-09 16:27:17 -0500

    Load compressed data directly.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 require 'progressbar'
 require 'db/seed_methods'
 require 'nokogiri'
-require 'debugger'
 include SeedMethods
-UBIOTA = "db/data/ubiota_taxonomy.psv"
-ANAGE = "db/data/anage.xml"
+UBIOTA = "db/data/ubiota_taxonomy.psv.bz2"
+ANAGE = "db/data/anage.xml.bz2"
 
 
 # Count the number of rows in a file.
-def num_lines(filename)
+def num_lines_bz2(filename)
   num_lines = 0
-  File.open(filename).each do |line|
+  puts "Calculating total size of job"
+  IO.popen("bunzip2 -c #{filename}").each do |line|
     id, term, rank, hierarchy, parent_id, num_children, hierarchy_ids = line.split("|")
     next if rank == "rank"
     break if rank == "6"
@@ -19,31 +19,35 @@ def num_lines(filename)
   num_lines
 end
 
+# Remove any existing taxa
+puts "Removing any existing taxa"
+Taxon.destroy_all
+
 # # Load new taxonomy information from UBioTa.
-# progress "Loading seed data...", num_lines(UBIOTA) do |progress_bar|
-#   File.open(UBIOTA).each do |line|
-#     id, term, rank, hierarchy, parent_id, num_children, hierarchy_ids = line.split("|")
-#     next if rank == "rank"
-#     break if rank == "6"
-#     taxon = Taxon.new
-#     taxon.id = id.to_i
-#     taxon.name = term
-#     taxon.rank = rank.to_i
-#     if parent_id == "-1"
-#       taxon.parent_id = nil
-#     else
-#       taxon.parent_id = parent_id.to_i
-#     end
-#     taxon.send(:create_without_callbacks)
-#     progress_bar.inc
-#   end
-# end
+progress "Loading seed data...", num_lines_bz2(UBIOTA) do |progress_bar|
+  IO.popen("bunzip2 -c #{UBIOTA}").each do |line|
+    id, term, rank, hierarchy, parent_id, num_children, hierarchy_ids = line.split("|")
+    next if rank == "rank"
+    break if rank == "6"
+    taxon = Taxon.new
+    taxon['id'] = id.to_i
+    taxon.name = term
+    taxon.rank = rank.to_i
+    if parent_id == "-1"
+      taxon.parent_id = nil
+    else
+      taxon.parent_id = parent_id.to_i
+    end
+    taxon.send(:create_without_callbacks)
+    progress_bar.inc
+  end
+end
 
 # Load species data from AnAge.
-puts "Loading AnAge data..."
-anage = Nokogiri::XML.parse(File.open(ANAGE))
-debugger
-num_lines = "heyo"
+# puts "Loading AnAge data..."
+# anage = Nokogiri::XML.parse(File.open(ANAGE))
+# debugger
+# num_lines = "heyo"
 
 
 
