dac7b06d134bed1e946f2c0fce516e926c223e66
Clinton Judy <clinton@westarete.com>
2009-12-16 16:21:44 -0500

    added the user show route 'account_path'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resource :user_session
+  map.account "/account", :controller => :users, :action => :show
   map.resources :users
   map.taxonomy "/taxonomy", :controller => :taxonomy, :action => :index
   map.resources :species
