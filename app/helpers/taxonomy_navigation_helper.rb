3bffe2057973ef8c3ff8f9f7d95f64a781f0804a
Clinton Judy <clinton@westarete.com>
2009-12-18 10:10:32 -0500

    Create starting taxonomy navigation dropdowns.

--- app/helpers/taxonomy_navigation_helper.rb
+++ app/helpers/taxonomy_navigation_helper.rb
@@ -1,2 +1,13 @@
 module TaxonomyNavigationHelper
+  
+  # Returns an array of paired ids and names for the taxonomy navigation
+  # dropdowns.
+  def dropdown_options(taxons=[])
+    # Map to sets of names and ids.
+    elements = taxons.map { |t| [t.name, t.id] }
+    # Prepend the "Any" option.
+    elements.unshift(['Any', ''])
+    options_for_select(elements)
+  end
+  
 end
--- spec/helpers/taxonomy_navigation_helper_spec.rb
+++ spec/helpers/taxonomy_navigation_helper_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
 describe TaxonomyNavigationHelper do
+  include TaxonomyNavigationHelper
 
-  #Delete this example and add some real ones or delete this file
-  it "should be included in the object returned by #helper" do
-    included_modules = (class << helper; self; end).send :included_modules
-    included_modules.should include(TaxonomyNavigationHelper)
+  describe "#dropdown_options" do
+    describe "when given some taxons" do      
+      before(:each) do
+        @bob = Taxon.create!(:name => 'bob')
+        @alice = Taxon.create!(:name => 'alice')
+      end
+      it "should return the html options, with an \"Any\" option prepended" do
+        dropdown_options([@bob, @alice]).should == 
+          "<option value=\"\">Any</option>\n" +
+          "<option value=\"#{@bob.id}\">bob</option>\n" +
+          "<option value=\"#{@alice.id}\">alice</option>"
+      end
+    end
+    describe "when given no arguments" do
+      it "should return a single html option for \"Any\"" do
+        dropdown_options.should == "<option value=\"\">Any</option>"
+      end
+    end
   end
-
+  
 end
