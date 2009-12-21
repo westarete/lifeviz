e16afadf949ccf7ba1c98e1b3a39f3e35b183260
Clinton Judy <clinton@westarete.com>
2009-12-21 16:30:43 -0500

    Annotated some old stuff.

--- spec/models/taxon_spec.rb
+++ spec/models/taxon_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: taxa
+#
+#  id          :integer         not null, primary key
+#  name        :string(255)
+#  parent_id   :integer
+#  lft         :integer
+#  rgt         :integer
+#  rank        :integer
+#  lineage_ids :string(255)
+#
+
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Taxon do
