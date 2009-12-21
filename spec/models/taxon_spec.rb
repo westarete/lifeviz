56e2d4d16c10631e7dccf571cffba5ceaa3b44d0
Clinton Judy <clinton@westarete.com>
2009-12-20 21:57:25 -0500

    Got rebuild_lineages method working and specced.

--- spec/models/taxon_spec.rb
+++ spec/models/taxon_spec.rb
@@ -5,16 +5,16 @@ describe Taxon do
   
   describe "#rebuild_lineage" do
     before(:each) do
+      Taxon.rebuild!
       @family = Taxon.find(6)
       @taxon = Taxon.new(:name => "Genus 2", :rank => 5)
       @taxon.save!
       @taxon.move_to_child_of(@family)
       # Set lft and rgt values for every taxon. Necessary!
-      Taxon.rebuild!
     end
     it "should set lineage_ids to the correct values" do
       @taxon.rebuild_lineage
-      @taxon.lineage_ids.should == "1,2,3"
+      @taxon.lineage_ids.should == "1,2,3,4,5,6"
     end
   end
   
