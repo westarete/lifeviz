eaed0be42ad6c9b4cbd2da8dc2a7d805d2c4330a
Clinton Judy <clinton@westarete.com>
2009-12-30 17:06:54 -0500

    Rename organism views and controller to species.

---
+++ app/controllers/species_controller.rb
@@ -0,0 +1,48 @@
+class SpeciesController < ApplicationController
+  before_filter :load_taxonomy
+  
+  def index
+    @species = Taxon.root.leaves.species
+  end
+  
+  def data
+    @taxon = Taxon.find(params[:taxon])
+    @species = @taxon.leaves.species
+    render :partial => "table"
+  end
+
+  def new
+    @species = Taxon.new
+  end
+
+  def create
+    @species = Taxon.new(params[:species])
+    if @species.save
+      flash[:success] = "Species saved."
+      redirect_to species_path(:id => @species.id)
+    else
+      flash[:failure] = "Species failed to save."
+      render :new
+    end
+  end
+
+  def show
+    @species = Taxon.find(params[:id])
+  end
+
+  def edit
+    @species = Taxon.find(params[:id])
+  end
+
+  def update
+    @species = Taxon.find(params[:id])
+    if @species.update_attributes(params[:species])
+      flash[:success] = "Species updated."
+      redirect_to species_path(:id => @species.id)
+    else
+      flash[:failure] = "Species failed to update."
+      render :update
+    end
+  end
+
+end
