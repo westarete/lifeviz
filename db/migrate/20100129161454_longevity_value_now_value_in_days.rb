80de47ccffdf00e708c39337fb0cf26704ec098d
John Marino <writejm@gmail.com>
2010-01-29 12:01:26 -0500

    changed value to be store as days... also converted to an integer

--- db/migrate/20100129161454_longevity_value_now_value_in_days.rb
+++ db/migrate/20100129161454_longevity_value_now_value_in_days.rb
@@ -1,9 +1,11 @@
 class LongevityValueNowValueInDays < ActiveRecord::Migration
   def self.up
-    change_column :value, :value_in_days, :integer    
+    rename_column :lifespans, :value, :value_in_days
+    change_column :lifespans, :value_in_days, :integer
   end
 
   def self.down
-    change_column :value, :value_in_days, :float
+    change_column :lifespans, :value_in_days, :float
+    rename_column :lifespans, :value_in_days, :value
   end
 end
