e9627d20a09ec43c846b6910128176b7e4841963
John Marino <writejm@gmail.com>
2010-01-29 13:54:36 -0500

    changed lifespan value to be numeric

--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100129165032) do
+ActiveRecord::Schema.define(:version => 20100129180101) do
 
   create_table "lifespans", :force => true do |t|
     t.integer  "species_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "value_in_days"
+    t.decimal  "value_in_days"
     t.string   "units"
   end
 
