40861f35271b53cc22057c4ba272004565b6bcf5
Scott Woods <scott@westarete.com>
2010-02-01 17:21:43 -0500

    Retrieve the user's total karma from the server.

--- spec/models/karma_client/karma_spec.rb
+++ spec/models/karma_client/karma_spec.rb
@@ -1,5 +1,15 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe KarmaClient::Karma do
+   before(:each) do
+     stub_karma_server
+     @karma = KarmaClient::Karma.new(User.make)
+   end
+  
+   describe "#total" do
+     it "should return the total karma for that user" do
+       @karma.total.should == 7
+     end
+   end
   
 end
