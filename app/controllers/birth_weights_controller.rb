c00fac129935025581d54a90042ad2ddb2d16e43
John Marino <writejm@gmail.com>
2010-06-12 04:07:45 -0400

    animated editing adult weight story

--- app/controllers/birth_weights_controller.rb
+++ app/controllers/birth_weights_controller.rb
@@ -16,6 +16,21 @@ class BirthWeightsController < ApplicationController
       render :new
     end
   end
+  
+  def edit
+    @birth_weight = BirthWeight.find(params[:id])
+  end
+  
+  def update
+    @birth_weight = BirthWeight.find(params[:id])
+    if @birth_weight.update_attributes(params[:birth_weight])
+      flash[:success] = "Birth weight updated."
+      redirect_to @species
+    else
+      flash.now[:failure] = "Birth weight update failed."
+      render :edit
+    end
+  end
     
 private
   
