5e5b55fb3d779e6d44fd8a2e2380335a8f154466
Clinton Judy <clinton@westarete.com>
2010-06-16 16:14:33 -0400

    Adding method to get lifespan in days, regardless of unit.

--- app/models/species.rb
+++ app/models/species.rb
@@ -31,18 +31,28 @@ class Species < Taxon
     move_to_child_of(parent)
   end
   
-  def lifespan
+  # Get the mode of the lifespans' units, and the average of the lifespan values, and return it in a string
+  def lifespan_with_units
     if lifespans.any?
       case lifespans.group_by(&:units).values.max_by(&:size).first.units
       when "Days"
-        (lifespans.collect(&:value_in_days).sum / lifespans.length.to_f).to_s + " Days"
+        "%.2f Days" % lifespan_in_days
       when "Months"
-        ((lifespans.collect(&:value_in_days).sum / lifespans.length.to_f) / 30.0).to_s + " Months"
+        "%.2f Months" % (lifespan_in_days / 30.0)
       when "Years"
-        ((lifespans.collect(&:value_in_days).sum / lifespans.length.to_f) / 365.0).to_s + " Years"
+        "%.2f Years" % (lifespan_in_days / 365.0)
       end
     else
       "N/A"
+    end
+  end
+  
+  # Return the average lifespan in days.
+  def lifespan_in_days
+    if lifespans.any?
+      lifespans.collect(&:value_in_days).sum / lifespans.length.to_f
+    else
+      0.0
     end
   end
   
