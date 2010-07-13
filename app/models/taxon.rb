301baaf0ff622f109743d14bf7b5a9486dfcadd6
John Marino <writejm@gmail.com>
2010-07-13 09:47:19 -0400

    organized code

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -15,6 +15,11 @@ class Taxon < ActiveRecord::Base
   validates_presence_of :name, :message => "can't be blank"
   
   RANK_LABELS = %w(Kingdom Phylum Class Order Family Genus Species)
+  
+  # This method rebuilds lineage_ids for the entire taxonomy.
+  def self.rebuild_lineages!
+    Taxon.find(1).rebuild_lineage_branch(nil) # Run rebuild_lineage on root node.
+  end
 
   def self.rebuild_stats
     rank = 5
@@ -30,77 +35,25 @@ class Taxon < ActiveRecord::Base
     end
   end
   
-  def precalculate_stats
-    children = self.children
-    if children.any?
-      self.avg_lifespan     = self.children.collect(&:avg_lifespan).delete_if{|x| x.nil?}.sum     / children.size.to_f
-      self.avg_litter_size  = self.children.collect(&:avg_litter_size).delete_if{|x| x.nil?}.sum  / children.size.to_f
-      self.avg_adult_weight = self.children.collect(&:avg_adult_weight).delete_if{|x| x.nil?}.sum / children.size.to_f
-      self.avg_birth_weight = self.children.collect(&:avg_birth_weight).delete_if{|x| x.nil?}.sum / children.size.to_f
-      self.save
-    end
+  # Redefining our own 'root' method, which should run much faster...
+  def self.root
+    find(1)
   end
   
-  def paginated_sorted_species(page)
-    begin
-      Species.paginate_by_sql("SELECT * FROM taxa WHERE lft >= #{self.lft} AND rgt <= #{self.rgt} AND rank = 6 ORDER BY name ASC", :page => page)
-    rescue
-      raise "Left and Right attributes were nil!"
-    end
-  end
-  
-  def species
-    begin
-      @species = Species.find_by_sql("SELECT * FROM taxa WHERE lft >= #{self.lft} AND rgt <= #{self.rgt} AND rank = 6 ORDER BY name ASC")
-    rescue
-      raise "Left and Right attributes were nil!"
-    end
-  end
-  
-  def complete_species
-    species.find_all {|s| s.all_data_available? }
+  def all_data_available?
+    avg_lifespan && avg_lifespan != 0 &&
+    avg_birth_weight && avg_birth_weight != 0 &&
+    avg_adult_weight && avg_adult_weight != 0 &&
+    avg_litter_size && avg_litter_size != 0
   end
   
   def parents
     lineage_ids.split(/,/).collect { |ancestor_id| Taxon.find(ancestor_id) }
   end
   
-  # Rebuild lineage_ids for this taxon.
-  def rebuild_lineage_ids
-    unless Taxon.first.blank? || parent_id.nil? || parent.lineage_ids.blank?
-      self.lineage_ids = (parent.lineage_ids + "," + parent_id.to_s)
-    end
-  end
-  
-  # Redefining our own 'root' method, which should run much faster...
-  def self.root
-    find(1)
-  end
-  
-  # This method rebuilds lineage_ids for the entire taxonomy.
-  def self.rebuild_lineages!
-    Taxon.find(1).rebuild_lineage_branch(nil) # Run rebuild_lineage on root node.
-  end
-  
-  # This is a recursive method to rebuild a tree of lineage_ids.
-  def rebuild_lineage_branch(parent_id, parent_lineage_ids="")
-    lineage_ids = if parent_id.nil?  # Root node
-                    ""
-                  elsif parent_lineage_ids == "" || parent_id == 1 # Child of root node
-                    "1"
-                  else
-                    parent_lineage_ids + "," + parent_id.to_s
-                  end     
-    
-    update_attributes(:lineage_ids => lineage_ids)
-    
-    unless leaf?
-      children.each {|child| child.rebuild_lineage_branch(id, lineage_ids)}
-    end
-  end
-  
-  def hierarchy
-    self.lineage_ids.split(',').map{|id| id.to_i}
+  # this method needs to be smarter... what if we want 2 ranks down?
+  def complete_species
+    self.children.find_all {|s| s.all_data_available? }
   end
   
   # returns an array of arrays. 
@@ -126,12 +79,67 @@ class Taxon < ActiveRecord::Base
     end
   end
   
+  def hierarchy
+    self.lineage_ids.split(',').map{|id| id.to_i}
+  end
+    
+  def paginated_sorted_species(page)
+    begin
+      Species.paginate_by_sql("SELECT * FROM taxa WHERE lft >= #{self.lft} AND rgt <= #{self.rgt} AND rank = 6 ORDER BY name ASC", :page => page)
+    rescue
+      raise "Left and Right attributes were nil!"
+    end
+  end  
+    
+  def precalculate_stats
+    children = self.children
+    if children.any?
+      self.avg_lifespan     = self.children.collect(&:avg_lifespan).delete_if{|x| x.nil?}.sum     / children.size.to_f
+      self.avg_litter_size  = self.children.collect(&:avg_litter_size).delete_if{|x| x.nil?}.sum  / children.size.to_f
+      self.avg_adult_weight = self.children.collect(&:avg_adult_weight).delete_if{|x| x.nil?}.sum / children.size.to_f
+      self.avg_birth_weight = self.children.collect(&:avg_birth_weight).delete_if{|x| x.nil?}.sum / children.size.to_f
+      self.save
+    end
+  end
+  
   def rank_in_words
     RANK_LABELS[self.rank.to_i]
   end
   
+  # This is a recursive method to rebuild a tree of lineage_ids.
+  def rebuild_lineage_branch(parent_id, parent_lineage_ids="")
+    lineage_ids = if parent_id.nil?  # Root node
+                    ""
+                  elsif parent_lineage_ids == "" || parent_id == 1 # Child of root node
+                    "1"
+                  else
+                    parent_lineage_ids + "," + parent_id.to_s
+                  end     
+    
+    update_attributes(:lineage_ids => lineage_ids)
+    
+    unless leaf?
+      children.each {|child| child.rebuild_lineage_branch(id, lineage_ids)}
+    end
+  end
+  
+  # Rebuild lineage_ids for this taxon.
+  def rebuild_lineage_ids
+    unless Taxon.first.blank? || parent_id.nil? || parent.lineage_ids.blank?
+      self.lineage_ids = (parent.lineage_ids + "," + parent_id.to_s)
+    end
+  end
+  
   def scientific_name 
     (id == 1) ? 'All Taxa' : read_attribute(:name)
+  end
+  
+  def species
+    begin
+      @species = Species.find_by_sql("SELECT * FROM taxa WHERE lft >= #{self.lft} AND rgt <= #{self.rgt} AND rank = 6 ORDER BY name ASC")
+    rescue
+      raise "Left and Right attributes were nil!"
+    end
   end
   
 end
