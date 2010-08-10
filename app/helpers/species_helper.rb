aa62fcf52c952bb9c5c14f96faad85c494c4f17b
Clinton Judy <clinton@westarete.com>
2010-08-10 00:06:49 -0400

    Removed last AJAX, turned dropdowns into static page navigation.

--- app/helpers/species_helper.rb
+++ app/helpers/species_helper.rb
@@ -65,19 +65,19 @@ module SpeciesHelper
   # dropdowns.
   def options_for_taxonomy_select(taxons=[], selected=nil)
     # Map to sets of names and ids.
-    elements = taxons.map { |t| [t.name, t.id] }
+    elements = taxons.map { |t| [t.name, "#{t.rank_in_words}/#{t.name}"] }
     # Prepend the "Any" option.
     elements.unshift(['Any', ''])
     options_for_select elements, selected
   end
   
   def taxon_dropdowns(taxon, ancestry)
-    lineage = taxon.hierarchy
-    lineage << taxon.id
+    lineage = taxon.parents
+    lineage << taxon
     returning String.new do |html|
       ancestry.each_with_index do |ancestor_taxa, rank|
         rank_in_words = ancestor_taxa.first.rank_in_words
-        html << controller.render_to_string(:partial => 'taxon_select', :layout => false, :locals => { :children => ancestor_taxa, :rank => rank, :rank_in_words => rank_in_words, :selected => lineage[rank + 1] })
+        html << controller.render_to_string(:partial => 'taxon_select', :layout => false, :locals => { :children => ancestor_taxa, :rank => rank, :rank_in_words => rank_in_words, :selected => lineage[rank + 1], :last => (rank + 1 == ancestry.length) })
       end
     end
   end
