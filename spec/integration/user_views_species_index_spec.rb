270eff8d3c272279a8f5edd96aab901815082107
Clinton Judy and John Marino <developers+clinton+john@westarete.com>
2010-09-24 16:07:49 -0400

    Uncomment integration specs. BROKEN.

--- spec/integration/user_views_species_index_spec.rb
+++ spec/integration/user_views_species_index_spec.rb
@@ -1,31 +1,31 @@
-# require 'spec_helper'
-# 
-# # make sure we have biological classification before we create species
-# make_biological_classification(5)
-# 
-# context "User viewing the species index page" do
-#   
-#   let(:species)       { Species.make(:parent_id => Taxon.find_by_rank(5).id ) }
-#   let(:other_species) { Species.make(:parent_id => Taxon.find_by_rank(5).id ) }
-#   
-#   before do
-#     species
-#     other_species    
-#     visit species_index_path
-#   end
-#   
-#   it "sees a list of species" do
-#     page.should have_xpath("//*[@id='species']//a[@href='#{species_path(species)}']", :text => species.name)
-#   end
-#   
-#   context "each species" do
-#     
-#     it "has a name" do
-#       save_and_open_page
-#       Species.all.each do |s|
-#         page.should have_xpath("//*[@class='name']", :text => species.name)
-#       end
-#     end
-#     
-#   end
-# end
+require 'spec_helper'
+
+# make sure we have biological classification before we create species
+make_biological_classification(5)
+
+context "User viewing the species index page" do
+  
+  let(:species)       { Species.make(:parent_id => Taxon.find_by_rank(5).id ) }
+  let(:other_species) { Species.make(:parent_id => Taxon.find_by_rank(5).id ) }
+  
+  before do
+    species
+    other_species    
+    visit species_index_path
+  end
+  
+  it "sees a list of species" do
+    page.should have_xpath("//*[@id='species']//a[@href='#{species_path(species)}']", :text => species.name)
+  end
+  
+  context "each species" do
+    
+    it "has a name" do
+      save_and_open_page
+      Species.all.each do |s|
+        page.should have_xpath("//*[@class='name']", :text => species.name)
+      end
+    end
+    
+  end
+end
